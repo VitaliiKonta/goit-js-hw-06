cd587d96dc8a71ab6366dfa9f268e5bb096ad539
VitaliiKonta <120408874+VitaliiKonta@users.noreply.github.com>
2023-04-20 21:57:04 +0300

    task-02 is ready

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulIngredients = document.querySelector("ul#ingredients");
+const liIngredientArray = [];
+
+ingredients.forEach((ingredient) => {
+  const liAdding = document.createElement("li");
+  liAdding.textContent = ingredient;
+  liAdding.classList.add("item");
+  liIngredientArray.push(liAdding);
+});
+
+ulIngredients.append(...liIngredientArray);
+console.log(ulIngredients);
