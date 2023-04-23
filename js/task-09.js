f155779a73af44a4f4a83a9ed5e585c3aa2c3330
VitaliiKonta <120408874+VitaliiKonta@users.noreply.github.com>
2023-04-23 21:04:49 +0300

    task-09  is ready

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const changeColorEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+bodyEl.addEventListener("click", changeBodyColorRandom);
+
+function changeBodyColorRandom() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorEl.textContent = `${getRandomHexColor()}`;
+}
